fde26922011a11e7391aef30c0a23c5f38a7c01a
Kia Farhang <kfarhang0@gmail.com>
2017-12-08 10:29:13 -0600

    Add top-level App component

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+      <story headline="Best story" image="https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg" byline="Yo Mama" chatter="Read this story!"/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Story from "./Story.vue";
+
+export default Vue.extend({
+  components: {
+    story: Story
+  }
+});
+</script>
+
+<style>
+.app {
+  width: 100%;
+}
+</style>
+
