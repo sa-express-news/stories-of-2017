2c734aafefacda3e783fd32bbfae5c71709a2b8c
Kia Farhang <kfarhang0@gmail.com>
2017-12-08 10:22:15 -0600

    Add single-file Vue component for Story

---
+++ src/components/Story.vue
@@ -0,0 +1,33 @@
+<template>
+        <div class='story'>
+          <img :src="image"/>
+            <h6>{{headline}}</h6>
+            <p class="byline">{{byline}}</p>
+            <p class="chatter">{{chatter}}</p>
+        </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    headline: {
+      type: String,
+      required: true
+    },
+    byline: {
+      type: String,
+      required: true
+    },
+    chatter: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
