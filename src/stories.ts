74ca12e4c1cd76f15d671d8a01c60414bba9a5d5
Kia Farhang <kfarhang0@gmail.com>
2017-12-09 10:07:49 -0600

    Create file to hold story data

---
+++ src/stories.ts
@@ -0,0 +1,22 @@
+const stories = [
+    {
+        "image": "http://ww2.hdnux.com/photos/67/75/44/14670097/13/920x1240.jpg",
+        "headline": "Sutherland Springs mass shooting",
+        "byline": "John Tedesco",
+        "chatter": "Sutherland Springs mass shooting: A gunman opens fire during worship services at the First Baptist Church in Sutherland Springs, a small rural community east of San Antonio. He kills 26 people (including an unborn child) and wounds 20. The gunman is then wounded by a local citizen with a semi-automatic rifle, then takes his own life, following a car chase. It is the worst mass shooting in Texas history."
+    },
+    {
+        "image": "http://ww2.hdnux.com/photos/67/75/44/14670097/13/920x1240.jpg",
+        "headline": "Hurricane Harvey",
+        "byline": "John Tedesco",
+        "chatter": "Harvey smashes into the Corpus Christi area as a Category 4 hurricane, then works its way up the Texas as a tropical storm dropping record amounts of rainfall from Victoria to Beaumont. A rain gauge near Houston recorded 51.88 inches of rain, which set a new record for rainfall from a single tropical storm or hurricane in the continental U.S. More than 1,600 evacuees are processed at San Antonio shelters. The death toll currently stands at 94 and the damage estimate is $190 billion."
+    },
+    {
+        "image": "http://ww2.hdnux.com/photos/67/75/44/14670097/13/920x1240.jpg",
+        "headline": "Trailer of Death",
+        "byline": "John Tedesco",
+        "chatter": "Responding to call in the early hours of July 23rd, police find 39 undocumented immigrants in a trailer in a Walmart parking lot. Immigrants interviewed by investigators said that anywhere between 70 and 200 people were crammed into the truck. Eight are dead at the scene, two later die of heat-related injuries. The truck driver plead guilty in October and faces up to life in prison."
+    }
+];
+
+export default stories;
