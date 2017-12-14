9ce551f1edbaf48dadace77618d3834801cbd5c1
Kia Farhang <kfarhang0@gmail.com>
2017-12-14 16:08:33 -0600

    Add header and footer components to overarching App component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="app">
-      <storylist />
+    <app-header />
+    <storylist />
+    <app-footer />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import AppHeader from "./Header.vue";
 import StoryList from "./StoryList.vue";
+import AppFooter from "./Footer.vue";
 
 export default Vue.extend({
   components: {
-    storylist: StoryList
+    storylist: StoryList,
+    appHeader: AppHeader,
+    appFooter: AppFooter
   }
 });
 </script>
